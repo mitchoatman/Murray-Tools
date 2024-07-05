904ad890fd6a8c6242bbaad78c8e0dddbc8b3858
mitchoatman <90937435+mitchoatman@users.noreply.github.com>
2024-07-05 09:15:00 -0700

    updated wall trimble commands

--- MurrayTools.tab/Point Layout.panel/Drop_Wall_Sleeve.pushbutton/Drop_Wall_Sleeve_script.py
+++ MurrayTools.tab/Point Layout.panel/Drop_Wall_Sleeve.pushbutton/Drop_Wall_Sleeve_script.py
@@ -1,10 +1,10 @@
-
 from Autodesk.Revit import DB
 from Autodesk.Revit.DB import FilteredElementCollector, Family, BuiltInCategory, FamilySymbol, LocationCurve, Transaction
 from Autodesk.Revit.UI.Selection import ObjectType
 from Parameters.Get_Set_Params import set_parameter_by_name, get_parameter_value_by_name_AsString, get_parameter_value_by_name_AsInteger
 import re
 from math import atan2, degrees
+from fractions import Fraction
 
 app = __revit__.Application
 doc = __revit__.ActiveUIDocument.Document
@@ -27,29 +27,23 @@ FamilyName = 'DR-WS'
 FamilyType = 'DR-WS'
 # Check if the family is in the project
 Fam_is_in_project = any(f.Name == FamilyName for f in families)
-#print("Family '{}' is in project: {}".format(FamilyName, is_in_project))
 
 family_pathCC = r'C:\Egnyte\Shared\BIM\Murray CADetailing Dept\REVIT\FAMILIES\Pipe Accessories\Sleeves\DR-WS.rfa'
 
 t = Transaction(doc, 'Load Trimble Wall Sleeve Family')
-#Start Transaction
 t.Start()
-if Fam_is_in_project == False:
+if not Fam_is_in_project:
     fload_handler = FamilyLoaderOptionsHandler()
     family = doc.LoadFamily(family_pathCC, fload_handler)
 
-#create a filtered element collector set to Category OST_PipeAccessory and Class FamilySymbol
-collector = FilteredElementCollector(doc)
-collector.OfCategory(BuiltInCategory.OST_PipeAccessory)
-collector.OfClass(FamilySymbol)
+collector = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_PipeAccessory).OfClass(FamilySymbol)
 
-famtypeitr = collector.GetElementIdIterator()
-famtypeitr.Reset()
-
-#Search Family Symbols in document.
-for item in famtypeitr:
-    famtypeID = item
-    famsymb = doc.GetElement(famtypeID)
+# Filter family symbols by family name and type name
+famsymb = None
+for fs in collector:
+    if fs.Family.Name == FamilyName and fs.get_Parameter(DB.BuiltInParameter.SYMBOL_NAME_PARAM).AsString() == FamilyType:
+        famsymb = fs
+        break
 
 if famsymb:
     famsymb.Activate()
@@ -57,9 +51,7 @@ if famsymb:
 
 t.Commit()
 
-#Family symbol name to place.
 symbName = 'DR-WS'
-
 
 def set_parameter_by_name(element, parameterName, value):
     element.LookupParameter(parameterName).Set(value)
@@ -70,19 +62,16 @@ def get_parameter_value_by_name(element, parameterName):
 def get_parameter_value_by_name_AsDouble(element, parameterName):
     return element.LookupParameter(parameterName).AsDouble()
 
-# Function to prompt user to select an MEP Fabrication Pipe
 def select_fabrication_pipe():
     selection = uidoc.Selection
     pipe_ref = selection.PickObject(ObjectType.Element, "Select an MEP Fabrication Pipe")
     pipe = doc.GetElement(pipe_ref.ElementId)
     return pipe
 
-# Function to prompt user to pick a point
 def pick_point():
     picked_point = uidoc.Selection.PickPoint("Pick a point along the centerline of the pipe")
     return picked_point
 
-# Function to get the centerline curve of the pipe
 def get_pipe_centerline(pipe):
     pipe_location = pipe.Location
     if isinstance(pipe_location, LocationCurve):
@@ -90,56 +79,58 @@ def get_pipe_centerline(pipe):
     else:
         raise Exception("The selected element does not have a valid centerline.")
 
-# Function to project point onto the centerline curve
 def project_point_on_curve(point, curve):
     result = curve.Project(point)
     return result.XYZPoint
 
-# Main function
 def place_and_modify_family(pipe, famsymb):
-    # Get the centerline of the pipe
     centerline_curve = get_pipe_centerline(pipe)
-    
-    # Prompt user to pick a point along the centerline
     picked_point = pick_point()
     
-    # Project the picked point onto the centerline curve
+    # Project the picked point onto the pipe centerline to get the Z coordinate
     projected_point = project_point_on_curve(picked_point, centerline_curve)
     
-    # Create new family in model at picked point
-    new_family_instance = doc.Create.NewFamilyInstance(projected_point, famsymb, DB.Structure.StructuralType.NonStructural)
+    # Create the insertion point using the X and Y from the picked point and Z from the projected point
+    insertion_point = DB.XYZ(picked_point.X, picked_point.Y, projected_point.Z)
     
-    # Set family diameter based on selected pipe
-    diameter = float(re.sub(r'[^\d.]', '', get_parameter_value_by_name_AsString(pipe, 'Overall Size'))) / 12 + 0.0833333
+    new_family_instance = doc.Create.NewFamilyInstance(insertion_point, famsymb, DB.Structure.StructuralType.NonStructural)
+
+    def frac2string(s):
+        i, f = s.groups(0)
+        f = Fraction(f)
+        return str(int(i) + float(f))
+
+    if '/' in get_parameter_value_by_name_AsString(pipe, 'Overall Size'):
+        diameter = float(re.sub(r'(?:(\d+)[-\s])?(\d+/\d+)[^\d.]', frac2string, get_parameter_value_by_name_AsString(pipe, 'Overall Size'))) / 12 + 0.0833333
+    else:
+        diameter = float(re.sub(r'[^\d.]', '', get_parameter_value_by_name_AsString(pipe, 'Overall Size'))) / 12 + 0.0833333
     set_parameter_by_name(new_family_instance, "Diameter", diameter)
-    
-    # Get location of inserted family
-    insertion_point = new_family_instance.Location.Point
     
     # Get connector location and angle on pipe
     pipe_connector = pipe.ConnectorManager.Connectors
     connector1, connector2 = list(pipe_connector)
     vec_x = connector2.Origin.X - connector1.Origin.X
     vec_y = connector2.Origin.Y - connector1.Origin.Y
-    angle = atan2(vec_y, vec_x)
-    axis = DB.Line.CreateBound(insertion_point, DB.XYZ(insertion_point.X, insertion_point.Y, insertion_point.Z + 1))
+    vec_z = connector2.Origin.Z - connector1.Origin.Z
+    angle_horizontal = atan2(vec_y, vec_x)
+    angle_vertical = atan2(vec_z, vec_x)
     
     # Set rotation on new family placed in model
-    DB.ElementTransformUtils.RotateElement(doc, new_family_instance.Id, axis, angle)
+    axis_horizontal = DB.Line.CreateBound(insertion_point, DB.XYZ(insertion_point.X, insertion_point.Y, insertion_point.Z + 1))
+    DB.ElementTransformUtils.RotateElement(doc, new_family_instance.Id, axis_horizontal, angle_horizontal)
+    
+    axis_vertical = DB.Line.CreateBound(insertion_point, DB.XYZ(insertion_point.X + 1, insertion_point.Y, insertion_point.Z))
+    DB.ElementTransformUtils.RotateElement(doc, new_family_instance.Id, axis_vertical, angle_vertical)
     
     # Set FP parameters on new family placed in model
     set_parameter_by_name(new_family_instance, 'FP_Service Name', get_parameter_value_by_name_AsString(pipe, 'Fabrication Service Name'))
     set_parameter_by_name(new_family_instance, 'FP_Service Abbreviation', get_parameter_value_by_name_AsString(pipe, 'Fabrication Service Abbreviation'))
 
-
-# Enter while loop for family placement
 while True:
     try:
-        # Start transaction for each family placement
         t = Transaction(doc, 'Place Trimble Wall Sleeve Family')
         t.Start()
         
-        # Select the MEP Fabrication Pipe
         pipe = select_fabrication_pipe()
         place_and_modify_family(pipe, famsymb)
         
